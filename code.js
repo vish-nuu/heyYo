74f572b15276d746ba04c8ac5bcd9ff65dc63276
vish-nuu <vishnu.clashonly@gmail.com>
2024-07-21 11:20:56 +0530

    secone

--- code.js
+++ code.js
@@ -2,8 +2,8 @@ var count = 0;
 var btnSize = 18;
 
 function noFunc() {
-  wordArray = ["dog", "cat", "bod", "sdflh", "dsf", "sdf", "sdfs", "jkjk"];
-  document.getElementById("btn2").innerHTML = `${wordArray[count]}`;
+  wordArray = ["nejamaa", "dog", "naayi", "peyy", "sethuru", "avlothana", "POOOO", "ahhhh"];
+  document.getElementById("btn2").innerHTML = `${wordArray[count%(len(Array))]}`;
   count++;
   document.getElementById("btn1").style.fontSize = `${(btnSize += 20)}px`;
 }
